d0ac2fc5db5613471f0ff7d7b3435b97f94ec668
jdredge <mrmightypants@gmail.com>
2015-10-25 17:16:03 -0400

    player moves jumps, plays flute; camera follows

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using FMOD.Studio;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,9 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed = 10;
     public float turningSpeed = 60;
     public float jumpForce = 5;
+    public float fluteReach = 100;
 
     private Rigidbody playerRigidBody;
-    private bool isGrounded;
     private FMOD.Studio.EventInstance fluteCall1; 
     private FMOD.Studio.ParameterInstance sustainingFlute;
 
@@ -24,20 +25,20 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // play flute sound effect if key just pressed, update position if held, stop if released
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            var attributes = FMOD.Studio.UnityUtil.to3DAttributes(transform.position);
-            fluteCall1.set3DAttributes(attributes);
-            fluteCall1.start();
+            // start the flute sound effect if key just pressed
+            PlayFlute(true);
+
         }
         else if (Input.GetKey(KeyCode.Q))
         {
-            var attributes = FMOD.Studio.UnityUtil.to3DAttributes(transform.position);
-            fluteCall1.set3DAttributes(attributes);;
+            // update the position of the sound effect if key is held
+            PlayFlute(false);
         }
         else if (Input.GetKeyUp(KeyCode.Q))
         {
+            // stop sound effect if released
             fluteCall1.stop(STOP_MODE.ALLOWFADEOUT);
         }
     }
@@ -62,6 +63,28 @@ public class PlayerMovement : MonoBehaviour
         //move
         transform.Rotate(0, h, 0);
         transform.Translate(0, 0, v);
+    }
+
+    void PlayFlute(bool keyDown)
+    {
+        // set the position of the sound effect to be the player's position
+        var attributes = FMOD.Studio.UnityUtil.to3DAttributes(transform.position);
+        fluteCall1.set3DAttributes(attributes);
+
+        if (keyDown)
+        {
+            fluteCall1.start();
+        }
+
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, fluteReach);
+        
+        foreach(Collider c in hitColliders){
+            
+            if (c.tag == "Alpaca")
+            {
+                // call alpaca's method
+            }
+        }
     }
 }
 
