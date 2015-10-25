0884b4ba0327cb2ecac763dd808fd2fe25c1f3d2
jdredge <mrmightypants@gmail.com>
2015-10-25 00:30:50 -0400

    player basic movement, jump, simple flute with sustain complete; camera follow and rotation complete; FMOD package and assets imported

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class CameraMovement : MonoBehaviour
+{
+	public GameObject target;			// target the camera focuses on
+	public float lookAngleOffset = 1;	// allows the target to be off-center in the camera's view
+	public float rotateSpeed = 5;		// speed of the camera's rotation
+	Vector3 offset;						// distance between the camera and target
+	
+	
+	void Start ()
+	{
+		//set up references
+		offset = target.transform.position - transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		// set the camera angle between its current angle and the angle of the target
+		float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = target.transform.eulerAngles.y;
+		float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime);
+
+		// add the mouse x axis to the angle so the player can adjust the rotation if desired
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		Quaternion rotation = Quaternion.Euler(0, angle - horizontal, 0);
+		transform.position = target.transform.position - (rotation * offset);
+
+		// aim the camera just above the target game object so that the target is not directly centered in the screen
+		Vector3 lookTarget = new Vector3(target.transform.position.x, target.transform.position.y + lookAngleOffset, target.transform.position.z);
+		transform.LookAt(lookTarget);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using FMOD.Studio;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float movementSpeed = 10;
+	public float turningSpeed = 60;
+	public float jumpForce = 5;
+
+	private Rigidbody playerRigidBody;
+	private bool isGrounded;
+	private FMOD.Studio.EventInstance fluteCall1; 
+	private FMOD.Studio.ParameterInstance sustainingFlute;
+
+	void Start()
+	{
+		//set up references
+		playerRigidBody = GetComponent<Rigidbody>();
+		fluteCall1 = FMOD_StudioSystem.instance.GetEvent("event:/sfx/player/flute1"); 
+		fluteCall1.getParameter("sustaining", out sustainingFlute);
+		sustainingFlute.setValue(0);
+	}
+	
+	
+	void Update()
+	{
+		// play flute sound effect if key just pressed, update position if held, stop if released
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			var attributes = FMOD.Studio.UnityUtil.to3DAttributes(transform.position);
+			fluteCall1.set3DAttributes(attributes);
+			fluteCall1.start();
+		}
+		else if (Input.GetKey(KeyCode.Q))
+		{
+			var attributes = FMOD.Studio.UnityUtil.to3DAttributes(transform.position);
+			fluteCall1.set3DAttributes(attributes);;
+		}
+		else if (Input.GetKeyUp(KeyCode.Q))
+		{
+			fluteCall1.stop(STOP_MODE.ALLOWFADEOUT);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// get input and call the Move method
+		float h = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+		float v = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		Move(h, v);
+	}
+	
+	
+	void Move(float h, float v)
+	{
+		// jump
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			playerRigidBody.AddForce(Vector3.up * jumpForce);
+		}
+
+		//move
+		transform.Rotate(0, h, 0);
+		transform.Translate(0, 0, v);
+	}
+
+}
