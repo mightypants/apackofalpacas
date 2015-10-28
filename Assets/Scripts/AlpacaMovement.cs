bd7b4635fad819b72009fedd1089a81d6065f656
jdredge <mrmightypants@gmail.com>
2015-10-28 11:13:36 -0400

    cleaned up bad tabs

--- Assets/Scripts/AlpacaMovement.cs
+++ Assets/Scripts/AlpacaMovement.cs
@@ -5,38 +5,38 @@ public class AlpacaMovement : MonoBehaviour
 {
     Transform target;
     NavMeshAgent nav;
-	bool isMoving;
-	
+    bool isMoving;
+    
     void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
-		isMoving = false;
+        isMoving = false;
     }
-	
+    
     void Update()
     {
-		if (isMoving)
-		{
-			float x = this.transform.position.x;
-			float z = this.transform.position.z;
-			float targetx = target.position.x;
-			float targetz = target.position.z;
-
-			if (x == targetx && z == targetz)
-			{
-				nav.SetDestination(this.transform.position);
-				isMoving = false;
-			}
-			else
-			{
-				nav.SetDestination(target.position);
-			}
-		}
+        if (isMoving)
+        {
+            float x = this.transform.position.x;
+            float z = this.transform.position.z;
+            float targetx = target.position.x;
+            float targetz = target.position.z;
+            
+            if (x == targetx && z == targetz)
+            {
+                nav.SetDestination(this.transform.position);
+                isMoving = false;
+            }    
+            else
+            {
+                nav.SetDestination(target.position);
+            }
+        }
     }
-
-	public void MoveTowardTarget(GameObject obj)
-	{
-		target = obj.transform;
-		isMoving = true;
-	}
+    
+    public void MoveTowardTarget(GameObject obj)
+    {
+        target = obj.transform;
+        isMoving = true;
+    }
 }
