83d7625710ecb866034adb5a30e345b0111ba2c4
James Sterritt <James Sterritt>
2015-11-12 13:47:37 -0500

    Updated via coping from alpaca_movement
    I have now added all the colliders, tweaked things to fit better,
    changed the Y on the templeswitch such that the mesh shows up,
    added as many textures as I can and have a couple odd texture
     patterning using unity.

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,23 +3,24 @@ using System.Collections;
 
 public class Switch : MonoBehaviour 
 {
-    public GameObject player;                       // Currently player-- will be remapped to alpaca.
     public GameObject switchTarget;                 // what the switch object is connected to.
     public string switchTargetAudio;                // the name (including path) of the FMOD sound effect the target will play 
-    public Texture activeTexture;                   // texture that is applied when switch activates game object
-    public Texture defaultTexture;                  // texture applied to game object normally (used to return to normal state). 
+    //public Texture activeTexture;                	// texture that is applied when switch activates game object
+    //public Texture defaultTexture;                // texture applied to game object normally (used to return to normal state). 
     public int requiredAlpacas = 1;                 // the number of alpacas needed to activate the switch
     
-    private Animation switchTargetAnimation;        // animation for target of switch (door animation, cage animation, etc).   
-    private Renderer[] switchTargetActiveColor;     // attaches to the color of the object, changing it's active color.
-    private string characterObjectTag = "Alpaca";   // tag switch searches for
+    //private Animation switchTargetAnimation;        // animation for target of switch (door animation, cage animation, etc).   
+    //private Renderer[] switchTargetActiveColor;     // attaches to the color of the object, changing it's active color.
+	private DoorLift switchTargetDoorLift;     		// attaches to the color of the object, changing it's active color.
+	private string characterObjectTag = "Alpaca";   // tag switch searches for
     private int alpacasPresent;                     // the number of alpacas currently on the switch
     
     
     void Start() 
     {
-    	switchTargetActiveColor = switchTarget.GetComponentsInChildren<Renderer>();
-        switchTargetAnimation = switchTarget.GetComponent<Animation>();
+    	//switchTargetActiveColor = switchTarget.GetComponentsInChildren<Renderer>();
+        //switchTargetAnimation = switchTarget.GetComponent<Animation>();
+		switchTargetDoorLift = switchTarget.GetComponent<DoorLift>();
         alpacasPresent = 0;
     }
     
@@ -29,19 +30,22 @@ public class Switch : MonoBehaviour
         {  
             // as soon as the alpaca hits the trigger, it should lock on to the switch and stay put
             AlpacaMovement alpaca = c.gameObject.GetComponent<AlpacaMovement>();
-            alpaca.MoveTowardTarget(gameObject);
+            StartCoroutine(alpaca.MoveTowardTarget(gameObject));
             alpacasPresent++;
             Debug.Log("An alpaca is present!! Number: " + alpacasPresent);
         
             if (alpacasPresent >= requiredAlpacas)
             {
                 FMOD_StudioSystem.instance.PlayOneShot(switchTargetAudio, switchTarget.transform.position);
-                switchTargetAnimation.Play();
+				StartCoroutine(switchTargetDoorLift.RaiseDoor());
+
+
+				//switchTargetAnimation.Play();
                 
-                foreach(Renderer r in switchTargetActiveColor)
-                {
-                    r.material.mainTexture = activeTexture;
-                }
+//                foreach(Renderer r in switchTargetActiveColor)
+//                {
+//                    r.material.mainTexture = activeTexture;
+//                }
             }
         } 
         else 
@@ -60,10 +64,10 @@ public class Switch : MonoBehaviour
         
         if (alpacasPresent <= requiredAlpacas)
         {
-            foreach (Renderer r in switchTargetActiveColor) 
-            {
-                r.material.mainTexture = defaultTexture;
-            }
+//            foreach (Renderer r in switchTargetActiveColor) 
+//            {
+//                r.material.mainTexture = defaultTexture;
+//            }
         }
     }
 }
