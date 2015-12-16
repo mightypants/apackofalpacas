43b2f5a6f25010d4430372fa6ecb3361b6c13738
jdredge <mrmightypants@gmail.com>
2015-12-15 20:44:36 -0500

    added script to play torch sfx

---
+++ Assets/Scripts/torchAudio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMOD.Studio;
+
+public class torchAudio : MonoBehaviour {
+
+	EventInstance torchEvent;
+
+
+	// Use this for initialization
+	void Start () {
+		torchEvent = FMOD_StudioSystem.instance.GetEvent("event:/sfx/environment/torch");
+
+		// set the position of the sound effect to be the player's position
+		var attributes = FMOD.Studio.UnityUtil.to3DAttributes(transform.position);
+		torchEvent.set3DAttributes(attributes);
+		torchEvent.start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
